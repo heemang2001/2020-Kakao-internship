b149fb2d79ff2e95f31fca55d67f14fba362047e
heemang2001 <babypooh2001@naver.com>
2021-06-23 13:18:07 +0900

    Add project files.

---
+++ 2020 카카오 인턴십/키패드 누르기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> numbers, string hand)
+{
+	string answer = "";
+
+
+
+	return answer;
+}
+
+int main()
+{
+	string strAnswer = solution({ 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 }, "right"); 
+	cout << strAnswer << '\n';
+
+	return 0;
+}
